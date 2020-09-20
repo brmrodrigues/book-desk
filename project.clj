310f3663236a728fb6d5a3e81fe4d932174fdf04
Bruno Rodrigues <lpbruno12@gmail.com>
2020-09-20 17:51:30 -0300

    malli + pedestal

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [metosin/jsonista "0.2.6"]
-                 [ring/ring-jetty-adapter "1.7.1"]
+                 [io.pedestal/pedestal.service "0.5.5"]
+                 [io.pedestal/pedestal.jetty "0.5.5"]
+                 [metosin/reitit-pedestal "0.5.5"]
                  [metosin/reitit "0.5.5"]]
   :jvm-opts ["-Dclojure.server.myrepl={:port,6666,:accept,clojure.core.server/repl}"]
   :main ^:skip-aot book-desk.core
---
+++ src/book_desk/server.clj
@@ -0,0 +1,130 @@
+(ns book-desk.server
+  (:require [io.pedestal.http :as server]
+            [reitit.ring :as ring]
+            [reitit.http :as http]
+            [reitit.coercion.malli]
+            [reitit.ring.malli]
+            [reitit.swagger :as swagger]
+            [reitit.swagger-ui :as swagger-ui]
+            [reitit.http.coercion :as coercion]
+            [reitit.dev.pretty :as pretty]
+            [reitit.http.interceptors.parameters :as parameters]
+            [reitit.http.interceptors.muuntaja :as muuntaja]
+            [reitit.http.interceptors.exception :as exception]
+            [reitit.http.interceptors.multipart :as multipart]
+            ;; Uncomment to use
+            ; [reitit.http.interceptors.dev :as dev]
+            ; [reitit.http.spec :as spec]
+            ; [spec-tools.spell :as spell]
+            [reitit.pedestal :as pedestal]
+            [clojure.core.async :as a]
+            [clojure.java.io :as io]
+            [muuntaja.core :as m]
+            [malli.util :as mu]))
+
+(def router
+  (pedestal/routing-interceptor
+    (http/router
+      [["/swagger.json"
+        {:get {:no-doc true
+               :swagger {:info {:title "Book a Desk API"
+                                :description "Book a desk safely ;)"}}
+               :handler (swagger/create-swagger-handler)}}]
+
+       ["/math"
+        {:swagger {:tags ["math"]}}
+
+        ["/plus"
+         {:get {:summary "plus with spec query parameters"
+                :parameters {:query [:map [:x int?] [:y int?]]}
+                :responses {200 {:body [:map [:total int?]]}}
+                :handler (fn [{{{:keys [x y]} :query} :parameters}]
+                           {:status 200
+                            :body {:total (+ x y)}})}
+          :post {:summary "plus with spec body parameters"
+                 :parameters {:body [:map [:x int?] [:y int?]]}
+                 :responses {200 {:body [:map [:total int?]]}}
+                 :handler (fn [{{{:keys [x y]} :body} :parameters}]
+                            {:status 200
+                             :body {:total (+ x y)}})}}]]]
+
+      {;:reitit.interceptor/transform dev/print-context-diffs ;; pretty context diffs
+       ;;:validate spec/validate ;; enable spec validation for route data
+       ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
+       :exception pretty/exception
+       :data {:coercion (reitit.coercion.malli/create
+                          {;; set of keys to include in error messages
+                           :error-keys #{#_:type :coercion :in :schema :value :errors :humanized #_:transformed}
+                           ;; schema identity function (default: close all map schemas)
+                           :compile mu/closed-schema
+                           ;; strip-extra-keys (effects only predefined transformers)
+                           :strip-extra-keys true
+                           ;; add/set default values
+                           :default-values true
+                           ;; malli options
+                           :options nil})
+              :muuntaja m/instance
+              :interceptors [;; swagger feature
+                             swagger/swagger-feature
+                             ;; query-params & form-params
+                             (parameters/parameters-interceptor)
+                             ;; content-negotiation
+                             (muuntaja/format-negotiate-interceptor)
+                             ;; encoding response body
+                             (muuntaja/format-response-interceptor)
+                             ;; exception handling
+                             (exception/exception-interceptor)
+                             ;; decoding request body
+                             (muuntaja/format-request-interceptor)
+                             ;; coercing response bodys
+                             (coercion/coerce-response-interceptor)
+                             ;; coercing request parameters
+                             (coercion/coerce-request-interceptor)
+                             ;; multipart
+                             (multipart/multipart-interceptor)]}})
+
+    ;; optional default ring handler (if no routes have matched)
+    (ring/routes
+      (swagger-ui/create-swagger-ui-handler
+        {:path "/"
+         :config {:validatorUrl nil
+                  :operationsSorter "alpha"}})
+      (ring/create-resource-handler)
+      (ring/create-default-handler))))
+
+(def service-map {:env :dev
+                  ::server/type :jetty
+                  ::server/port 3000
+                  ::server/join? false
+                  ;; no pedestal routes
+                  ::server/routes []
+                  ;; allow serving the swagger-ui styles & scripts from self
+                  ::server/secure-headers {:content-security-policy-settings
+                                           {:default-src "'self'"
+                                            :style-src "'self' 'unsafe-inline'"
+                                            :script-src "'self' 'unsafe-inline'"}}})
+
+(defn start []
+  (-> {:env :dev
+       ::server/type :jetty
+       ::server/port 3000
+       ::server/join? false
+       ;; no pedestal routes
+       ::server/routes []
+       ;; allow serving the swagger-ui styles & scripts from self
+       ::server/secure-headers {:content-security-policy-settings
+                                {:default-src "'self'"
+                                 :style-src "'self' 'unsafe-inline'"
+                                 :script-src "'self' 'unsafe-inline'"}}}
+      (server/default-interceptors)
+      ;; use the reitit router
+      (pedestal/replace-last-interceptor router)
+      (server/dev-interceptors)
+      (server/create-server)
+      (server/start))
+  (println "server running in port 3000"))
+
+(comment
+  (start))
+
+; (server/stop service-map)
